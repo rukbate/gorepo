4bf726f7200ea88442d25075eb1ef25e3374d494
lin <rukbate@hotmail.com>
2018-11-15 16:22:10 +0800

    regular expression matching - dynamic programming

--- leetcode/p10/regexMatching.go
+++ leetcode/p10/regexMatching.go
@@ -5,46 +5,69 @@ import "fmt"
 func isMatch(s string, p string) bool {
 	i := 0
 	j := 0
+
+	arr := make([][]int, len(s), len(s))
+	for x := 0; x < len(s); x++ {
+		brr := make([]int, len(p), len(p))
+		arr[x] = brr
+	}
 	
-	return match(s, p, i, j)
+	return match(s, p, i, j, arr)
 }
 
-func match(s string, p string, i int, j int) bool {
-	if i == len(s) && j == len(p) {
-		return true
-	} else if i < len(s) && j < len(p) {
-		if j + 1 < len(p) && p[j + 1] != '*' {
-			return matchByte(s[i], p[j]) && match(s, p, i + 1, j + 1)
-		} else if j + 1 < len(p) && p[j + 1] == '*' {
-			return (matchByte(s[i], p[j]) && match(s, p, i + 1, j + 2)) || (matchByte(s[i], p[j]) && match(s, p, i + 1, j)) || match(s, p, i, j + 2)
-		} else {
-			return i + 1 == len(s) && matchByte(s[i], p[j])
-		}
-	} else if j < len(p) {
-		a := 0
-		b := 0
-		for j < len(p) {
-			if p[j] != '*' {
-				a++
-			} else {
-				b++
-			}
+func match(s string, p string, i int, j int, arr [][]int) bool {
+	if i < len(s) && j < len(p) && arr[i][j] != 0 {
+		return arr[i][j] == 1
+	} 
 
-			j++
+	n, k := nextUnit(p, j)
+	y := false
+	if i >= len(s) {
+		y = (k == -1 || (len(n) == 2 && match(s, p, i, k, arr)))
+	} else if len(n) == 1 {
+		y = matchUnit(s[i:i+1], n) && match(s, p, i + 1, k, arr)
+	} else if len(n) == 2 {
+		y = match(s, p, i, k, arr) || (matchUnit(s[i: i + 1], n) && (match(s, p, i + 1, k, arr) || match(s, p, i + 1, k - 2, arr)))
+	} else {
+		y = i >= len(s)
+	}
+
+	if( i < len(s) && j < len(p)) {
+		if y {
+			arr[i][j] = 1
+		} else {
+			arr[i][j] = -1
 		}
-		return a == b
+	}
+
+	return y
+}
+
+func nextUnit(s string, i int) (string, int) {
+	if i < len(s) {
+		if i + 1 < len(s) && s[i + 1] == '*' {
+			return s[i: i + 2], i + 2
+		} 
+	
+		return (string)(s[i]), i + 1		
+	}
+	
+	return "", -1	
+}
+
+func matchUnit(s string, p string) bool {
+	if len(p) == 1 {
+		return len(s) == 1 && (s[0] == p[0] || p[0] == '.')
+	} else if len(p) == 2 {
+		return len(s) == 0 || (s[0] == p[0] || p[0] == '.')
 	} else {
 		return false
 	}
 }
 
-func matchByte(a byte, b byte) bool {
-	return a == b || b == '.'
-}
-
 func main() {
-	s := "a"
-	p := "ab*"
+	s := "ab"
+	p := "d*c*a*b*"
 
 	fmt.Println(isMatch(s, p))
 }
