c9d85483e963f417e8a1a7d2858cf084026cf1e5
lin <rukbate@hotmail.com>
2018-11-17 11:22:28 +0800

    two sum

---
+++ leetcode/p1/twoSum.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	cache := make(map[int]int)
+	
+	for i := 0; i < len(nums); i++ {
+		val, ok := cache[target - nums[i]]
+		if ok {
+			return []int {val, i};
+		}
+
+		cache[nums[i]] = i
+	}
+
+	return nil
+}
+
+func main() {
+	nums := []int {2, 7, 11, 15}
+	target := 9
+
+	fmt.Println(twoSum(nums, target))
+}
