6830605ce7930e6bb7b95cebe2590920018d194c
lin <rukbate@hotmail.com>
2018-11-17 11:36:47 +0800

    longest substring without repeating characters

---
+++ leetcode/p3/longestSubstringWithoutRepeatingCharacters.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func lengthOfLongestSubstring(s string) int {
+	n := len(s)
+	res := 0
+
+	cache := make(map[byte]int)
+	for i, j := 0, 0; j < n; j++ {
+		c, ok := cache[s[j]]
+		if ok {
+			if c > i {
+				i = c
+			}
+		}
+
+		if j - i + 1 > res {
+			res = j - i + 1
+		}
+		cache[s[j]] = j + 1
+	}
+
+	return res
+}
+
+func main() {
+	s := "abcabcbb"
+	fmt.Println(lengthOfLongestSubstring(s))
+}
